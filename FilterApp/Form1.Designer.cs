48d74c2444506e17f7f6c3f52e120ec5f0566758
Nahidef <erdincfidan18@gmail.com>
2025-05-03 23:14:02 +0300

    Add project files.

---
+++ FilterApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FilterApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.btnLoadImage = new System.Windows.Forms.Button();
+			this.btnApplyFilter = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(262, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(222, 204);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(292, 237);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(151, 24);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// btnLoadImage
+			// 
+			this.btnLoadImage.Location = new System.Drawing.Point(248, 303);
+			this.btnLoadImage.Name = "btnLoadImage";
+			this.btnLoadImage.Size = new System.Drawing.Size(105, 23);
+			this.btnLoadImage.TabIndex = 2;
+			this.btnLoadImage.Text = "Görsel Yükle";
+			this.btnLoadImage.UseVisualStyleBackColor = true;
+			this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+			// 
+			// btnApplyFilter
+			// 
+			this.btnApplyFilter.Location = new System.Drawing.Point(383, 303);
+			this.btnApplyFilter.Name = "btnApplyFilter";
+			this.btnApplyFilter.Size = new System.Drawing.Size(101, 23);
+			this.btnApplyFilter.TabIndex = 3;
+			this.btnApplyFilter.Text = "Filtre Uygula";
+			this.btnApplyFilter.UseVisualStyleBackColor = true;
+			this.btnApplyFilter.Click += new System.EventHandler(this.btnApplyFilter_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnApplyFilter);
+			this.Controls.Add(this.btnLoadImage);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button btnLoadImage;
+		private System.Windows.Forms.Button btnApplyFilter;
+	}
+}
+
---
+++ FilterApp/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace FilterApp
+{
+	public partial class Form1 : Form
+	{
+		private Bitmap originalImage; 
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			comboBox1.Items.Add("Griye Çevir");
+			comboBox1.Items.Add("Gaussian Blur");
+			comboBox1.Items.Add("Kenar Algılama");
+			comboBox1.SelectedIndex = 0;
+			pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+			pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+		}
+
+		private void btnLoadImage_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "Resim Dosyaları|*.jpg;*.png;*.bmp";
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				originalImage = new Bitmap(ofd.FileName); 
+				pictureBox1.Image = originalImage;
+			}
+		}
+
+		private void btnApplyFilter_Click(object sender, EventArgs e)
+		{
+			if (originalImage == null) return;
+
+			Bitmap filtered = null;
+			string selectedFilter = comboBox1.SelectedItem.ToString();
+
+			try
+			{
+				switch (selectedFilter)
+				{
+					case "Griye Çevir":
+						filtered = ToGrayScale(originalImage);
+						break;
+					case "Ortalama (Mean) Filtresi":
+						filtered = MeanFilter(originalImage);
+						break;
+					case "Gauss Filtresi":
+						filtered = GaussianFilter(originalImage);
+						break;
+					case "Kenar Tespiti (Sobel)":
+						filtered = SobelEdgeDetection(originalImage);
+						break;
+				}
+
+				if (filtered != null)
+					pictureBox1.Image = filtered;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Hata: {ex.Message}");
+			}
+		}
+
+		
+		private Bitmap ToGrayScale(Bitmap source)
+		{
+			Bitmap bmp = new Bitmap(source.Width, source.Height);
+			for (int y = 0; y < source.Height; y++)
+			{
+				for (int x = 0; x < source.Width; x++)
+				{
+					Color c = source.GetPixel(x, y);
+					int gray = (int)((c.R + c.G + c.B) / 3);
+					bmp.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+				}
+			}
+			return bmp;
+		}
+
+		
+		private Bitmap MeanFilter(Bitmap source)
+		{
+			Bitmap bmp = new Bitmap(source.Width, source.Height);
+			int[,] kernel = {
+				{ 1, 1, 1 },
+				{ 1, 1, 1 },
+				{ 1, 1, 1 }
+			};
+
+			int kernelSum = 9;
+
+			for (int y = 1; y < source.Height - 1; y++)
+			{
+				for (int x = 1; x < source.Width - 1; x++)
+				{
+					int r = 0, g = 0, b = 0;
+
+					for (int ky = -1; ky <= 1; ky++)
+					{
+						for (int kx = -1; kx <= 1; kx++)
+						{
+							Color c = source.GetPixel(x + kx, y + ky);
+							r += c.R * kernel[ky + 1, kx + 1];
+							g += c.G * kernel[ky + 1, kx + 1];
+							b += c.B * kernel[ky + 1, kx + 1];
+						}
+					}
+
+					r /= kernelSum;
+					g /= kernelSum;
+					b /= kernelSum;
+
+					bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+				}
+			}
+			return bmp;
+		}
+
+		
+		private Bitmap GaussianFilter(Bitmap source)
+		{
+			Bitmap bmp = new Bitmap(source.Width, source.Height);
+			int[,] kernel = {
+				{ 1, 2, 1 },
+				{ 2, 4, 2 },
+				{ 1, 2, 1 }
+			};
+
+			int kernelSum = 16;
+
+			for (int y = 1; y < source.Height - 1; y++)
+			{
+				for (int x = 1; x < source.Width - 1; x++)
+				{
+					int r = 0, g = 0, b = 0;
+
+					for (int ky = -1; ky <= 1; ky++)
+					{
+						for (int kx = -1; kx <= 1; kx++)
+						{
+							Color c = source.GetPixel(x + kx, y + ky);
+							r += c.R * kernel[ky + 1, kx + 1];
+							g += c.G * kernel[ky + 1, kx + 1];
+							b += c.B * kernel[ky + 1, kx + 1];
+						}
+					}
+
+					r /= kernelSum;
+					g /= kernelSum;
+					b /= kernelSum;
+
+					bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+				}
+			}
+			return bmp;
+		}
+
+		
+		private Bitmap SobelEdgeDetection(Bitmap source)
+		{
+			Bitmap gray = ToGrayScale(source);
+			Bitmap result = new Bitmap(gray.Width, gray.Height);
+
+			int[,] gx = {
+				{ -1, 0, 1 },
+				{ -2, 0, 2 },
+				{ -1, 0, 1 }
+			};
+
+			int[,] gy = {
+				{ -1, -2, -1 },
+				{  0,  0,  0 },
+				{  1,  2,  1 }
+			};
+
+			for (int y = 1; y < gray.Height - 1; y++)
+			{
+				for (int x = 1; x < gray.Width - 1; x++)
+				{
+					int gxSum = 0, gySum = 0;
+
+					for (int ky = -1; ky <= 1; ky++)
+					{
+						for (int kx = -1; kx <= 1; kx++)
+						{
+							int intensity = gray.GetPixel(x + kx, y + ky).R;
+							gxSum += gx[ky + 1, kx + 1] * intensity;
+							gySum += gy[ky + 1, kx + 1] * intensity;
+						}
+					}
+
+					int gVal = Math.Min(255, Math.Abs(gxSum) + Math.Abs(gySum));
+					result.SetPixel(x, y, Color.FromArgb(gVal, gVal, gVal));
+				}
+			}
+
+			return result;
+		}
+	}
+}
